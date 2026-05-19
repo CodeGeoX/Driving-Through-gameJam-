cda2a4d4cc0fef8b51d3daff6b99d69d29504611
JunliangHu <jhu1@edu.tecnocampus.cat>
2026-05-19 19:27:30 +0200

    v1

--- Driving Through GameJam/Driving Through GameJam/Program.cs
+++ Driving Through GameJam/Driving Through GameJam/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Driving_Through_GameJam;
 
-Console.WriteLine("Hello, World!");
+namespace TcGame
+
+{
+    using SFML.Graphics;
+    using SFML.Window;
+    using SFML.System;
+    public class Program
+    {
+        public static void Main()
+        {
+            VideoMode v = new VideoMode(640*2, 480*2);
+            RenderWindow rw = new RenderWindow(v, "Sprite Sample");
+            
+            Engine.Initialize();
+            
+            while (rw.IsOpen)
+            {
+                Engine.Get.Scene.Create<Player>();
+            }
+        }
+    }
+}
