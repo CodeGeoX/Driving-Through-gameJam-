c444bf3e31a18e5d13c4c5676df36c6b9060c2b8
mvericatg <mvericatg@edu.tecnocampus.cat>
2026-05-19 19:08:15 +0200

    v2

--- Driving Through GameJam/Driving Through GameJam/Game/Player.cs
+++ Driving Through GameJam/Driving Through GameJam/Game/Player.cs
@@ -1,6 +1,51 @@
-﻿namespace Driving_Through_GameJam.Game;
+﻿using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+using System.Collections.Generic;
+using System;
 
-public class Player
+namespace TcGame
 {
+    public class Player : StaticActor
+    {
+        Random alea = new Random();
+        Vector2f Target;
+        private float coolDown = 3;
+        public Player()
+        {
+      
+            Layer = ELayer.Middle;
+      
+            int skin = alea.Next(1, 4);
+      
+            switch (skin)
+            {
+                case 1:
+                    Sprite = new Sprite(new Texture("Data/Textures/rui1.png"));
+                    break;
+                case 2:
+                    Sprite = new Sprite(new Texture("Data/Textures/rui2.png"));
+                    break;
+                case 3:
+                    Sprite = new Sprite(new Texture("Data/Textures/rui3.png"));
+                    break;
+                default:
+                    break;
+            }
+            Center();
+            Position = new Vector2f(780.0f, 350.0f);
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+            Forward = (Engine.Get.MousePos - Position).Normal();
+            Rotation = (float) Math.Atan2(Forward.Y, Forward.X) * MathUtil.RAD2DEG;
+      
+            
+      
+      
+        }
     
+    }
 }
--- Driving Through GameJam/Driving Through GameJam/Program.cs
+++ Driving Through GameJam/Driving Through GameJam/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using TcGame;
+
+namespace Driving_Through_GameJam.Game
+{
+    public class App
+    {
+        public static void Main()
+        {
+            Engine.Get.Run(MyGame.Get);
+        }
+    }
+}
